d9882ce5e40523173a403c8c55ae3fc8baf9b60d
ledungcobra <ledungcobra@gmail.com>
2025-02-06 08:30:37 +0700

    feat: add test cases

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
@@ -1,21 +1,35 @@
 package se.magnus.microservices.core.product.services;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import se.magnus.util.http.ServiceUtil;
 import se.magnus.util.http.core.product.Product;
 import se.magnus.util.http.core.product.ProductService;
+import se.magnus.util.http.exceptions.InvalidInputException;
+import se.magnus.util.http.exceptions.NotFoundException;
 
 @RestController
-@FieldDefaults(makeFinal = true)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @AllArgsConstructor
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
 	ServiceUtil serviceUtil;
 
 	@Override
 	public Product getProduct(int productId) {
+		log.debug( "/product return the found product for productId={}", productId );
+
+		if ( productId < 1 ) {
+			throw new InvalidInputException( "Invalid productId: " + productId );
+		}
+
+		if ( productId == 13 ) {
+			throw new NotFoundException( "No product found for productId: " + productId );
+		}
 		return new Product(productId, "name" + productId, 123, serviceUtil.getServiceAddress());
 	}
 }
